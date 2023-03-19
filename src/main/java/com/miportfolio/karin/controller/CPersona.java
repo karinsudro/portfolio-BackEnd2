08d4ae8af90b13929418baacbd20c37c7472f773
karinsudro <karinsudro@gmail.com>
2023-03-19 02:01:51 -0300

    pondo auth

--- src/main/java/com/miportfolio/karin/controller/CPersona.java
+++ src/main/java/com/miportfolio/karin/controller/CPersona.java
@@ -66,7 +66,7 @@ public class CPersona {
     }
     
     //Login. en local fciona bien
-    @PostMapping ("auth/login")   
+    @PostMapping ("auth/login") 
     @ResponseBody
     public PersonaDto loginPersona(@RequestBody Persona perso) {
         return persoServ.loginPersona(perso.getEmail(), perso.getClave());
